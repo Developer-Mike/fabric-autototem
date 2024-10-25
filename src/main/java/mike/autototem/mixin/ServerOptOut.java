b1b4d82ae815380e8004e73f28f9da1779edc721
Jones <admin@jonesdev.xyz>
2024-10-25 15:55:38 +0200

    Fix invalid payload packet

--- src/main/java/mike/autototem/mixin/ServerOptOut.java
+++ src/main/java/mike/autototem/mixin/ServerOptOut.java
@@ -1,9 +1,10 @@
 package mike.autototem.mixin;
 
 import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.network.packet.BrandCustomPayload;
+import net.minecraft.network.packet.UnknownCustomPayload;
 import net.minecraft.network.packet.c2s.common.CustomPayloadC2SPacket;
 import net.minecraft.network.packet.s2c.play.GameJoinS2CPacket;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,11 +15,13 @@ public class ServerOptOut {
     @Inject(at = @At("TAIL"), method = "onGameJoin")
     private void sendInfoPackage(GameJoinS2CPacket packet, CallbackInfo ci) {
         ClientPlayNetworkHandler networkHandler = (ClientPlayNetworkHandler) ((Object) this);
-        
+
         networkHandler.sendPacket(
-            new CustomPayloadC2SPacket(
-                new BrandCustomPayload("autototem")
-            )
+                new CustomPayloadC2SPacket(
+                        // Use a custom channel for compatibility with https://github.com/jonesdevelopment/sonar
+                        // and other anti-cheat, anti-exploit, and anti-bot plugins.
+                        new UnknownCustomPayload(Identifier.of("autototem-fabric"))
+                )
         );
     }
 }
