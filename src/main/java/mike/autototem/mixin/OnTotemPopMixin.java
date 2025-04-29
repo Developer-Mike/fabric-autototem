d518c848f863e4f2f9f96abad498fc588378b128
Developer-Mike <delta.cephei@outlook.com>
2025-04-29 09:18:08 +0200

    Updated to 1.21.5

--- src/main/java/mike/autototem/mixin/OnTotemPopMixin.java
+++ src/main/java/mike/autototem/mixin/OnTotemPopMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.client.render.GameRenderer;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.network.packet.Packet;
@@ -14,6 +15,8 @@ import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
 import net.minecraft.network.packet.c2s.play.UpdateSelectedSlotC2SPacket;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.SlotActionType;
+import net.minecraft.screen.sync.ComponentChangesHash.ComponentHasher;
+import net.minecraft.screen.sync.ItemStackHash;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import org.spongepowered.asm.mixin.Mixin;
@@ -21,6 +24,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import it.unimi.dsi.fastutil.ints.Int2ObjectArrayMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 
 import java.util.ArrayList;
@@ -66,12 +71,12 @@ public class OnTotemPopMixin {
         }
 
         System.out.println("Restocking Totem");
-        restockSlot(player, spareTotemSlot);
+        restockSlot(client, player, spareTotemSlot);
     }
 
     private int getSlotWithSpareTotem(PlayerInventory inventory) {
-        for (int i = 0; i < inventory.main.size(); i++) {
-            ItemStack stack = inventory.main.get(i);
+        for (int i = 0; i < inventory.size(); i++) {
+            ItemStack stack = inventory.getStack(i);
 
             if (!stack.isEmpty() && stack.getItem() == Items.TOTEM_OF_UNDYING) {
                 return i;
@@ -81,13 +86,16 @@ public class OnTotemPopMixin {
         return -1;
     }
 
-    private void restockSlot(PlayerEntity player, int totemSlot) {
-        PlayerInventory playerInventory = player.getInventory();
+    private void restockSlot(MinecraftClient client, PlayerEntity player, int totemSlot) {
+        PlayerInventory inventory = player.getInventory();
+        int currentHotbarSlot = inventory.getSelectedSlot();
         packetsToSend = new ArrayList<>();
 
         if (totemSlot < 9) {
             // Select Totem Slot
-            packetsToSend.add(new UpdateSelectedSlotC2SPacket(totemSlot));
+            if (currentHotbarSlot != totemSlot) {
+                packetsToSend.add(new UpdateSelectedSlotC2SPacket(totemSlot));
+            }
 
             // Move Totem To Offhand
             packetsToSend.add(new PlayerActionC2SPacket(
@@ -97,32 +105,47 @@ public class OnTotemPopMixin {
             ));
 
             // Restore Old Hotbar Slot
-            packetsToSend.add(new UpdateSelectedSlotC2SPacket(playerInventory.selectedSlot));
+            if (currentHotbarSlot != totemSlot) {
+                packetsToSend.add(new UpdateSelectedSlotC2SPacket(currentHotbarSlot));
+            }
         } else {
-            // Move Current Hotbar Slot To Offhand
+            int availableHotbarSlot = inventory.getEmptySlot();
+            if (availableHotbarSlot < 9) {
+                // Select the future totem slot
+                packetsToSend.add(new UpdateSelectedSlotC2SPacket(availableHotbarSlot));
+            } else {
+                // Move Current Hotbar Slot To Offhand
+                packetsToSend.add(new PlayerActionC2SPacket(
+                  PlayerActionC2SPacket.Action.SWAP_ITEM_WITH_OFFHAND,
+                  BlockPos.ORIGIN, 
+                  Direction.DOWN
+                ));
+            }
+
+            // Move Totem To Selected Hotbar Slot
+            ScreenHandler screenHandler = player.currentScreenHandler;
+            ClientPlayNetworkHandler networkHandler = client.getNetworkHandler();
+            packetsToSend.add(new ClickSlotC2SPacket(
+              screenHandler.syncId,
+              screenHandler.getRevision(),
+              (short) totemSlot,
+              (byte) 0,
+              SlotActionType.QUICK_MOVE,
+              new Int2ObjectOpenHashMap<>(),
+              ItemStackHash.fromItemStack(inventory.getSelectedStack().copy(), networkHandler.method_68823())
+            ));
+
+            // The Hotbar Slot contains the Totem - Move it to the Offhand and restore the old item
             packetsToSend.add(new PlayerActionC2SPacket(
               PlayerActionC2SPacket.Action.SWAP_ITEM_WITH_OFFHAND,
               BlockPos.ORIGIN, 
               Direction.DOWN
             ));
 
-            // Move Totem To Selected Hotbar Slot
-            ScreenHandler screenHandler = player.currentScreenHandler;
-            packetsToSend.add(new ClickSlotC2SPacket(
-              screenHandler.syncId,
-              screenHandler.getRevision(),
-              totemSlot,
-              0,
-              SlotActionType.QUICK_MOVE,
-              playerInventory.getStack(playerInventory.selectedSlot).copy(),
-              new Int2ObjectOpenHashMap<ItemStack>()
-            ));
-
-            // The Hotbar Slot contains the Totem - Move it to the Offhand and restore the old item
-            packetsToSend.add(new PlayerActionC2SPacket(
-              PlayerActionC2SPacket.Action.SWAP_ITEM_WITH_OFFHAND,
-              BlockPos.ORIGIN, Direction.DOWN
-            ));
+            // Restore the old selected hotbar slot
+            if (availableHotbarSlot < 9) {
+                packetsToSend.add(new UpdateSelectedSlotC2SPacket(currentHotbarSlot));
+            }
         }
     }
 }
